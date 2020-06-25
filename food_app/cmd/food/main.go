52d67db6b0d65545a58653bca294d6dd47dc80c6
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2020-06-25 12:47:53 +0000

    get all foods successfully implemented

--- food_app/cmd/food/main.go
+++ food_app/cmd/food/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
+	"food_app/food_app/models"
 	"food_app/food_app/restapi"
 	"food_app/food_app/restapi/operations"
+	"food_app/food_app/restapi/operations/foods"
 	"food_app/food_app/restapi/operations/sample_description"
 
 	"log"
 
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/go-openapi/swag"
 )
+
+var dummyFoodSource = make(map[int64]*models.Food)
+
+func allItems(since int64, limit int32) (result []*models.Food) {
+	result = make([]*models.Food, 0)
+	for id, food := range dummyFoodSource {
+		if len(result) >= int(limit) {
+			return
+		}
+		if since == 0 || id > since {
+			result = append(result, food)
+		}
+	}
+	return
+}
 
 func main() {
 	// load embedded swagger spec from a JSON file
@@ -32,6 +50,19 @@ func main() {
 			return sample_description.NewShowGreetingsToTheAPIOK()
 		})
 
+	// implement find all foods
+	api.FoodsGetFoodsHandler = foods.GetFoodsHandlerFunc(
+		func(parameters foods.GetFoodsParams) middleware.Responder {
+			mergedParams := foods.NewGetFoodsParams()
+			mergedParams.Since = swag.Int64(0)
+			if parameters.Since != nil {
+				mergedParams.Since = parameters.Since
+			}
+			if parameters.Limit != nil {
+				mergedParams.Limit = parameters.Limit
+			}
+			return foods.NewGetFoodsOK().WithPayload(allItems(*mergedParams.Since, *mergedParams.Limit))
+		})
 	// serve
 	if err := server.Serve(); err != nil {
 		log.Fatalln(err)
